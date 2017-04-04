7e884d0f45db598db5776c6b63056e82d4e8111d
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-04-04 11:35:55 +0530

    Add: Exercise3 Ui Polish

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -19,7 +19,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -32,37 +35,77 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
+    private TextView mErrorTextView;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
+        /*
+         * Using findViewById, we get a reference to our TextView from xml. This allows us to
+         * do things like set the text of the TextView.
+         */
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+
+        mErrorTextView = (TextView) findViewById(R.id.errorDisplay);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressDisplay);
+
+        /* Once all of our views are setup, we can load the weather data. */
         loadWeatherData();
     }
 
+    /**
+     * This method will get the user's preferred location for weather, and then tell some
+     * background method to get the weather data in the background.
+     */
     private void loadWeatherData() {
+        showWeatherDataVIew();
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new FetchWeatherTask().execute(location);
     }
 
+
+    private void showWeatherDataVIew() {
+        mErrorTextView.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void  showErrorMessage() {
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected void onPreExecute() {
+            mErrorTextView.setVisibility(View.INVISIBLE);
+            mWeatherTextView.setVisibility(View.INVISIBLE);
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
 
         @Override
         protected String[] doInBackground(String... params) {
+
+            /* If there's no zip code, there's nothing to look up. */
             if (params.length == 0) {
                 return null;
             }
 
             String location = params[0];
-            URL requestUrl = NetworkUtils.buildUrl(location);
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
 
             try {
-                String jsonResponse = NetworkUtils.getResponseFromHttpUrl(requestUrl);
+                String jsonWeatherResponse = NetworkUtils
+                        .getResponseFromHttpUrl(weatherRequestUrl);
 
-                String[] weatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, jsonResponse);
-                return weatherData;
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleJsonWeatherData;
+
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
@@ -70,30 +113,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String[] result) {
-            if (result != null) {
-                for (String weatherString: result) {
-                    mWeatherTextView.append(weatherString + "\n\n\n");
+        protected void onPostExecute(String[] weatherData) {
+
+            mProgressBar.setVisibility(View.INVISIBLE);
+
+            if (weatherData != null) {
+                showWeatherDataVIew();
+                /*
+                 * Iterate through the array and append the Strings to the TextView. The reason why we add
+                 * the "\n\n\n" after the String is to give visual separation between each String in the
+                 * TextView. Later, we'll learn about a better way to display lists of data.
+                 */
+                for (String weatherString : weatherData) {
+                    mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
             }
+            else {
+                showErrorMessage();
+            }
+
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.forecast, menu);
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.forecast, menu);
+        /* Return true so that the menu is displayed in the Toolbar */
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_refresh) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
             mWeatherTextView.setText("");
             loadWeatherData();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-
     }
 }
